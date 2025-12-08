aace9db213ef25a623b1a952aa1d2cbbcbb30041
Kai Hsieh <kaihsieh@cayintech.com>
2025-12-08 12:03:02 +0800

    1 add provider configurations

--- 1_use_aws_provider/main.tf
+++ 1_use_aws_provider/main.tf
@@ -10,6 +10,18 @@ terraform {
   }
 }
 
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs#provider-configuration
+# 使用特定 aws credential
+provider "aws" {
+  alias  = "application"
+  # profile = "test"
+  region = "ap-southeast-1"
+  # access_key = "my-access-key"
+  # secret_key = "my-secret-key"
+  # shared_config_files      = ["/Users/tf_user/.aws/conf"]
+  # shared_credentials_files = ["/Users/tf_user/.aws/creds"]
+}
+
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/caller_identity
 data "aws_caller_identity" "current" {}
 
